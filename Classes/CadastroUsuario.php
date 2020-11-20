0a66f1d960bb75df2d7c78fc7f17def1d002b793
WendelSilvestre <wendelsilvestre9@gmail.com>
2020-11-20 11:56:22 -0300

    Atualização Classe: CadastroUsuario
    - Pequenos ajustes em nomes
    - Adição de um erro que possa nos ajudar

--- Classes/CadastroUsuario.php
+++ Classes/CadastroUsuario.php
@@ -4,13 +4,12 @@ Class CadastroUsuario
 {
   private $pdo;
 
-
   public function cadastrar($nome, $CPF, $email, $endereço, $senha){
 
     global $pdo;
 
     //verificar se ja existe o email cadastrado
-    $sql = $pdo->prepare("SELECT id_usuario FROM usuario_professor WHERE email =:e");
+    $sql = $pdo->prepare("SELECT ID_Usuario FROM usuario_professor WHERE Email =:e");
     $sql->bindValue(":e", $email);
     $sql->execute();
     if($sql->rowCount() > 0){
@@ -18,7 +17,7 @@ Class CadastroUsuario
     }
     else{
       //caso nao exista, cadastrar
-      $sql = $pdo->prepare("INSERT INTO usuario_professor (Nome, CPF_Usuario, Email, Endereço, Senha) VALUES (:n, :c, :e, :en, :s)");
+      $sql = $pdo->prepare("INSERT INTO usuario_professor (NickName, CPF_Usuario, Email, Endereço, Senha) VALUES (:n, :c, :e, :en, :s)");
         $sql->bindValue(":n", $nome);
         $sql->bindValue(":c", $CPF);
         /* $sql->bindValue(":m", $materia); */
@@ -36,7 +35,7 @@ Class CadastroUsuario
 
     global $pdo;
     //verificar se o email e senha estao cadastrados
-    $sql = $pdo->prepare("SELECT id_usuario FROM usuario_professor WHERE email = :e AND senha = :s");
+    $sql = $pdo->prepare("SELECT ID_Usuario FROM usuario_professor WHERE Email = :e AND senha = :s");
     $sql->bindValue(":e", $email);
     $sql->bindValue(":s", md5($senha));
     $sql->execute();
@@ -44,7 +43,7 @@ Class CadastroUsuario
       //entrar no sistema
       $dado = $sql->fetch();
       session_start();
-      $_SESSION['id_usuario'] = $dado['id_usuario'];
+      $_SESSION['ID_Usuario'] = $dado['ID_Usuario'];
       return true; //logado com sucesso
     }
     else{
