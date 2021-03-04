ed4fd5a80df80d5e5e422bf307bc9f4d693a46ff
Mightyfox777 <65208868+NicolasHenriques@users.noreply.github.com>
2021-03-04 15:48:19 -0300

    Att no PDF
    - Agora o gabarito aparece
    - Nome do documento e do autor estão certos

--- Classes/PDF.php
+++ Classes/PDF.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-// Include the main TCPDF library (search for installation path).
+
+// Include da biblioteca TCPFD
 require_once('TCPDF/tcpdf.php');
 require_once('TCPDF/config/tcpdf_config.php');
 require_once'../Classes/Documento.php';
@@ -8,15 +9,14 @@ $d = new Documento;
 require_once'../Classes/DataBase.php';
 $u = new DataBase;
 
-// create new PDF document
+//Criando um documento PDF
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 001');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+//Colocando informações no documento
+$pdf->SetCreator($_SESSION["NickName"]);
+$pdf->SetAuthor($_SESSION["NickName"]);
+$pdf->SetTitle($_SESSION["nome_documento"]);
+$pdf->setPrintHeader(false);
 
 // set default monospaced font
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
@@ -44,29 +44,27 @@ if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 $pdf->setFontSubsetting(true);
 
 // Set font
-// dejavusans is a UTF-8 Unicode font, if you only need to
-// print standard ASCII chars, you can use core fonts like
-// helvetica or times to reduce file size.
-$pdf->SetFont('dejavusans', '', 14, '', true);
 
-// Add a page
-// This method has several options, check the source code documentation for more information.
+$pdf->SetFont('helvetica', '', 14, '', true);
+
+//Adicionando a página de questões do documento
 $pdf->AddPage();
 
 // set text shadow effect
 $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
 
-// Set some content to print
+//cabeçalho do documento
 $html = <<<EOD
 <p>Nome: </p>
 <p>Prontuário: Turma:</p>
 <p>Professor: $_SESSION[NickName] Data:</p>
 EOD;
 
-// Print text using writeHTMLCell()
-$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+//inserindo o cabeçalho no documento
+$pdf->writeHTMLCell(0, 0, '', '', $html, 1, 1, 0, true, '', true);
 
 // ---------------------------------------------------------
+//inserindo as questões no documento
 $u->conectar();
 global $pdo;
 $i = 0;
@@ -82,18 +80,46 @@ if (isset($_SESSION['id_documento'])){
   $results = $sql->fetchAll(\PDO::FETCH_ASSOC);
   foreach($results as $row){
 
-     $html ="<p>".$row['Enunciado']."</p>";
-     $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+    $i++;
+    $html ="<p style=text-align: justify;><b>" .$i.".". "</b> ".$row['Enunciado']."</br></br></p>";
+    $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
    }
   }
 
+//verificando se o usuário quer gabarito ou não
 
-$html ="<p>".$i."</p>";
+if ($_SESSION["gabarito"]==1){
 
-$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+  //adicionando a página do gabarito
+  $pdf->addPage();
+
+  global $pdo;
+  $i = 0;
+
+  if (isset($_SESSION['id_documento'])){
+
+    $idDocumento = $_SESSION['id_documento'];
+
+    $sql = $pdo->prepare("SELECT Texto FROM alternativa as alt INNER JOIN questao as q ON alt.Questao_ID_Questao_ = q.ID_Questao_ AND q.ID_Questao_ IN (SELECT Questao_ID_Questao_ FROM questao_documento WHERE Documento_ID_Documento = :d)");
+    $sql->bindValue(":d", $idDocumento);
+    $sql->execute();
+    if($sql->rowCount() > 0){
+      while($m = $sql->fetch()){
+      $i++;
+      $html =" <p style=text-align: justify;><b>" .$i.". ". "</b>" .$m['Texto']."</br></br></p>";
+      $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+      }
+    }
+  }
+}
+
+//$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+
 // Close and output PDF document
 // This method has several options, check the source code documentation for more information.
-$pdf->Output('example_001.pdf', 'I');
+$pdf->Output($_SESSION["nome_documento"].'.pdf', 'I');
 
 //============================================================+
 // END OF FILE
